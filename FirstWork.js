6edc8b273938f804af9625e0887a2c5256c3f377
CemileIsmayilZada <5cctf7f@code.edu.az>
2022-10-28 01:36:32 +0400

    tasks

---
+++ FirstWork.js
@@ -0,0 +1,41 @@
+//Task 1: 
+let temperature=34;
+if(temperature<15){
+    console.log("Hava Soyuqdur");
+}
+else if(temperature>15){
+    console.log("Hava Istidir");
+}
+else{
+    console.log("hava ne isti nede soyuqdu");
+}
+//Task 2
+
+let number=23;
+
+if(number%2==0){
+    console.log("Number is Even");
+
+}
+else{
+    console.log("Number is Odd");
+}
+
+//Task 3
+
+let numb1=42;
+let numb2=23;
+let numb3=23;
+
+if((numb1<numb2)&&(numb1<numb3))
+{
+    console.log("Minimum number is "+numb1);
+}
+else if((numb2<numb1)&&(numb2<numb3)){
+    console.log("Minimum number is "+numb2);
+}
+else{
+    console.log("Minimum number is "+numb3);
+}
+
+
